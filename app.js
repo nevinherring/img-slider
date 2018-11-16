68f49dbc6f54d4c9d72f185455bb580d824a9613
Nevin Herring <nevin.herring@gmail.com>
2018-11-15 22:23:32 -0600

    created js infinite loop for images and fixed css

--- app.js
+++ app.js
@@ -8,5 +8,21 @@ nextBtn.addEventListener('click', nextSlide);
 prevBtn.addEventListener('click', prevSlide);
 
 function nextSlide(){
+    container.animate([{opacity:'0.1'},{opacity:'1.0'}],{duration:1000, fill:'forwards'});
+    if(counter === 4){
+        counter = -1;
+    }
     counter++;
+
+    container.style.backgroundImage = `url(img/bcg-${counter}.jpeg)`
+}
+
+function prevSlide(){
+    container.animate([{opacity:'0.1'},{opacity:'1.0'}],{duration:1000, fill:'forwards'});
+    if(counter === 0){
+        counter = 5;
+    }
+    counter--;
+
+    container.style.backgroundImage = `url(img/bcg-${counter}.jpeg)`
 }
