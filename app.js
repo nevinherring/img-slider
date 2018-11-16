9df02580cbd30d3ca428edf0e1834cb454994814
Nevin Herring <nevin.herring@gmail.com>
2018-11-15 22:13:49 -0600

    added const and eventListeners

---
+++ app.js
@@ -0,0 +1,12 @@
+const nextBtn = document.querySelector('.nextBtn');
+const prevBtn = document.querySelector('.prevBtn');
+const container = document.querySelector('.images');
+
+let counter = 0;
+
+nextBtn.addEventListener('click', nextSlide);
+prevBtn.addEventListener('click', prevSlide);
+
+function nextSlide(){
+    counter++;
+}
